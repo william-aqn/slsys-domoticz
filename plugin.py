d3ba22271dc392077627c0d18ceabb3b24cb6744
william-aqn <dark.crimson.d.c.r.m@gmail.com>
2023-02-17 20:22:02 +0300

    upd

--- plugin.py
+++ plugin.py
@@ -3,13 +3,15 @@
 #           Author:     DCRM, 2023
 #
 """
-<plugin key="Slsys" name="SLS gateway" author="DCRM" version="0.0.1" wikilink="https://slsys.github.io/Gateway/README_rus.html" externallink="https://github.com/william-aqn/slsys-domoticz">
+<plugin key="Slsys" name="SLS gateway" author="DCRM" version="1.0.0" wikilink="https://slsys.github.io/Gateway/README_rus.html" externallink="https://github.com/william-aqn/slsys-domoticz">
     <params>
         <param field="Address" label="IP Address" width="200px" required="true" default="192.168.1.20"/>
         <param field="Port" label="Port" width="30px" required="true" default="80"/>
         <param field="Password" label="Token" width="250px" default=""/>
 
-        <param field="Mode2" label="Radio" width="240px" default="jazz#http://jazz.streamr.ru/jazz-64.mp3|http://jazz.streamr.ru/jazz-64.mp3"/>
+        <param field="Mode2" label="Radio" width="540px" default="http://jazz.streamr.ru/jazz-64.mp3|http://jazz.streamr.ru/jazz-128.mp3"/>
+        
+        <param field="Mode5" label="Heartbeat" width="30px" default="60"/>
         
         <param field="Mode6" label="Debug" width="75px">
             <options>
@@ -23,6 +25,7 @@
 import Domoticz
 import sys
 import json
+import time
 from urllib import request, parse
 
 
@@ -43,12 +46,31 @@ class BasePlugin:
     }
     
     def buildUrlSelectorOptions(self):
-        Options = {"LevelActions": "||||",
-                       "LevelNames": "Auto|Silent|Medium|High",
-                       "LevelOffHidden": "false",
-                       "SelectorStyle": "0"
-                      }
+        Options = {
+            "LevelNames": "Off|"+Parameters["Mode2"],
+            "LevelOffHidden": "true",
+            "SelectorStyle": "1"
+        }
+        Domoticz.Debug("buildUrlSelectorOptions "+str(Options))
         return Options
+    
+    def getLevelNamesFromDevice(self, device):
+        mapLevels={}
+        levelIndex=0
+        levelNames = device.Options['LevelNames'].split('|')
+        for levelName in levelNames:
+            #Domoticz.Debug("LevelNames "+str(levelName)+"")
+            mapLevels[levelIndex]=levelName
+            levelIndex+=10
+        #Domoticz.Debug("mapLevels "+str(mapLevels)+"")
+        return mapLevels
+    
+    def findLevelByNameFromDevice(self, device, name):
+        mapLevels = self.getLevelNamesFromDevice(device)
+        for level in mapLevels:
+            if mapLevels[level]==name:
+                return level
+        return False
     
     def onStart(self):
         if Parameters["Mode6"] == "Debug":
@@ -63,10 +85,12 @@ class BasePlugin:
             Domoticz.Device(Name="Volume", Unit=self.UNITS['Volume'], TypeName='Dimmer').Create()
             
         if self.UNITS['UrlSelector'] not in Devices:
-            Domoticz.Device(Name="Radio stations", Unit=self.UNITS['UrlSelector'], TypeName='Selector Switch', Options={}).Create()
+            Domoticz.Device(Name="Url", Unit=self.UNITS['UrlSelector'], TypeName='Selector Switch', Options={}).Create()
+            
         # Обновляем станции
         if self.UNITS['UrlSelector'] in Devices:
-            Devices[self.UNITS['UrlSelector']].Update(Options=self.buildUrlSelectorOptions())
+            urlSelectorDevice = Devices[self.UNITS['UrlSelector']]
+            urlSelectorDevice.Update(nValue=urlSelectorDevice.nValue, sValue=str(urlSelectorDevice.sValue), Options = self.buildUrlSelectorOptions())
                                     
         if self.UNITS['Reboot'] not in Devices:
             Domoticz.Device(Name="Reboot", Unit=self.UNITS['Reboot'], TypeName='Push Off').Create()
@@ -82,31 +106,35 @@ class BasePlugin:
         if (Parameters["Port"] == "443"): 
             self.sProtocol = "https"
 
-        Domoticz.Connection(Name="SlsPing", Transport="TCP/IP", Protocol=self.sProtocol, Address=Parameters["Address"], Port=Parameters["Port"]).Connect()
+        # Domoticz.Connection(Name="SlsPing", Transport="TCP/IP", Protocol=self.sProtocol, Address=Parameters["Address"], Port=Parameters["Port"]).Connect()
 
         # Опросим шлюз
         self.status()
         
         # Период обновления
-        Domoticz.Heartbeat(30)
+        if (Parameters["Mode5"]):
+            Domoticz.Heartbeat(int(Parameters["Mode5"]))
+        
         return True
     
     def onConnect(self, Connection, Status, Description):
-        if (Status == 0):
-            Domoticz.Log("Connected successfully to: "+Connection.Address+":"+Connection.Port)
-        else:
-            Domoticz.Log("Failed to connect ("+str(Status)+") to: "+Connection.Address+":"+Connection.Port)
-            Domoticz.Debug("Failed to connect ("+str(Status)+") to: "+Connection.Address+":"+Connection.Port+" with error: "+Description)
-            # Turn devices off in Domoticz
-            for Key in Devices:
-                UpdateDevice(Key, 0, Devices[Key].sValue)
+        # if (Status == 0):
+        #     Domoticz.Log("Connected successfully to: "+Connection.Address+":"+Connection.Port)
+        # else:
+        #     Domoticz.Log("Failed to connect ("+str(Status)+") to: "+Connection.Address+":"+Connection.Port)
+        #     Domoticz.Debug("Failed to connect ("+str(Status)+") to: "+Connection.Address+":"+Connection.Port+" with error: "+Description)
+        #     # Turn devices off in Domoticz
+        #     for Key in Devices:
+        #         UpdateDevice(Key, 0, Devices[Key].sValue)
         return True
     
     # Синхронизируем устройства
     def SyncDevices(self):
         UpdateDevice(self.UNITS['Audio'], self.audioState, self.audioUrl)
         UpdateDevice(self.UNITS['Volume'], self.audioState, ""+str(self.audioVolumeLevel)+"")
-    
+        
+        UpdateDevice(self.UNITS['UrlSelector'], self.audioState, self.findLevelByNameFromDevice(Devices[self.UNITS['UrlSelector']], self.audioUrl))
+        
     # Отправить /api GET запрос на SLS шлюз
     def send(self, payload):
         try:
@@ -122,6 +150,7 @@ class BasePlugin:
             req = request.Request(self.sProtocol+'://' + Parameters["Address"] + ':' + Parameters["Port"] + '/api/'+url)
             response = request.urlopen(req, timeout=3).read()
             response = response.decode("utf-8", "ignore")
+            time.sleep(200/1000) # Задержка - что бы шлюз не насиловать
             Domoticz.Debug('Response - ' + str(response) )
             return json.loads(response)
         except Exception as e:
@@ -133,41 +162,40 @@ class BasePlugin:
         self.send('scripts?action=evalCode&plain=os.restart()')
         
     def audioPlay(self, url):
+        self.audioStop()
+        # Остановим если Off
+        if (url=='Off'):
+            return False
+        # Запускаем поток
         self.audioUrl=url
         self.send('audio?action=play&url='+url)
-        self.audioStatus()
-        self.audioUrlStatus()
         
     def audioStop(self):
         self.send('audio?action=stop')
-        self.audioStatus()
         
     def audioVolumeSet(self, level):
         self.send('audio?action=setvolume&value='+str(level))
-        self.audioVolumeStatus()
         
     def audioStatus(self):
         result = self.send('audio?action=getstatus')
         if result:
             self.audioState = result['result']
-            self.SyncDevices()
         
     def audioVolumeStatus(self):
         result = self.send('audio?action=getvolume')
         if result:
             self.audioVolumeLevel = result['result']
-            self.SyncDevices()
         
     def audioUrlStatus(self):
         result = self.send('audio?action=geturl')
         if result:
             self.audioUrl = result['result']
-            self.SyncDevices()
         
     def status(self):
         self.audioStatus()
         self.audioVolumeStatus()
         self.audioUrlStatus()
+        self.SyncDevices()
         
     def onMessage(self, Connection, Data):
         Domoticz.Debug("onMessage "+str(Data)+"")
@@ -178,11 +206,14 @@ class BasePlugin:
         Domoticz.Log("onCommand called for Unit " + str(Unit) + ": Parameter '" + str(Command) + "', Level: " + str(Level) + "', Hue: " + str(Hue))
         Command = Command.strip()
         
-        # Останавливаем аудио
+        # Останавливаем/Воспроизводим аудио
         if (self.UNITS['Audio']==Unit):
-            self.audioStop()
+            if (Command=='On' and self.audioUrl!=""):
+                self.audioPlay(self.audioUrl)
+            else:
+                self.audioStop()
             
-        # Меняем громкость
+        # Меняем/Заглушаем громкость
         if (self.UNITS['Volume']==Unit):
             if (Command=='Off'): #"Set Level"
                 Level = 0
@@ -192,6 +223,13 @@ class BasePlugin:
         if (self.UNITS['Reboot']==Unit):
             self.reboot()
             
+        # Воспроизводим радио
+        if (self.UNITS['UrlSelector']==Unit):
+            # Находим ссылки
+            mapLevels = self.getLevelNamesFromDevice(Devices[Unit])
+            self.audioPlay(mapLevels[Level])
+
+        self.status()
         return True
 
     def onNotification(self, Name, Subject, Text, Status, Priority, Sound, ImageFile):
@@ -210,13 +248,6 @@ class BasePlugin:
         Domoticz.Log("onStop called")
         return True
 
-    def ClearDevices(self):
-        # Stop everything and make sure things are synced
-        self.audioState = 1
-        self.audioUrl = ""
-
-        self.SyncDevices()
-        return
         
 global _plugin
 _plugin = BasePlugin()
